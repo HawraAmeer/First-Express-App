3daf06db3ba862c9f440768c50c52f4388cec6d1
HawraAmeer <hwra.ameer97@gmail.com>
2021-02-22 11:53:29 +0300

    Added sign in & sign up

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,7 +9,16 @@ exports.signup = async (req, res, next) => {
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     req.body.password = hashedPassword;
     const newUser = await User.create(req.body);
-    res.status(201).json({ message: "User created successfully" });
+    const payload = {
+      id: newUser.id,
+      username: newUser.username,
+      email: newUser.email,
+      firstName: newUser.firstName,
+      lastName: newUser.lastName,
+      exp: Date.now() + 900000,
+    };
+    const token = jwt.sign(JSON.stringify(payload), "asupersecretkey");
+    res.json({ token });
   } catch (error) {
     next(error);
   }
@@ -20,6 +29,9 @@ exports.signin = (req, res) => {
   const payload = {
     id: user.id,
     username: user.username,
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
     exp: Date.now() + 900000,
   };
   const token = jwt.sign(JSON.stringify(payload), "asupersecretkey");
